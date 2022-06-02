2179bfe3ff24a110baa79df1730a1c4d26c8ef89
ochitos <395174+ganoch@users.noreply.github.com>
2022-06-02 18:04:53 +0800

    refactor: Types.cs inteface -> abstract class

--- GolomtEcommerse/Types.cs
+++ GolomtEcommerse/Types.cs
@@ -2,9 +2,14 @@
 namespace GolomtEcommerse
 {
 
-    public interface Request
+    public abstract class Request
     {
-        void computeHash(string secret);
+        public string checksum { get; set; } = string.Empty;
+
+        protected void computeHash(string secret, string data)
+        {
+            this.checksum = Utils.GetHMAC(secret, data);
+        }
     }
 
     public class CreateInvoiceRequest : Request
@@ -15,7 +20,6 @@ namespace GolomtEcommerse
         public string returnType { get; set; }
         public string callback { get; set; }
         public bool getToken { get; set; }
-        public string checksum { get; set; }
 
         public CreateInvoiceRequest(float amount, string transactionId, string returnType, string callback, bool getToken)
         {
@@ -24,31 +28,26 @@ namespace GolomtEcommerse
             this.callback = callback;
             this.getToken = getToken;
             this.returnType = returnType;
-            this.checksum = "";
         }
 
         public void computeHash(string secret)
         {
-            string data = transactionId + amount + returnType + callback;
-            checksum = Utils.GetHMAC(secret, data);
+            this.computeHash(secret, transactionId + amount + returnType + callback);
         }
     }
 
     public class InquiryRequest : Request
     {
         public string transactionId { get; set; }
-        public string checksum { get; set; }
 
         public InquiryRequest(string transactionId)
         {
             this.transactionId = transactionId;
-            this.checksum = "";
         }
 
         public void computeHash(string secret)
         {
-            string data = transactionId + transactionId;
-            checksum = Utils.GetHMAC(secret, data);
+            this.computeHash(secret, transactionId + transactionId);
         }
     }
 
@@ -58,7 +57,6 @@ namespace GolomtEcommerse
         public string invoice { get; set; }
         public string transactionId { get; set; }
         public string token { get; set; }
-        public string checksum { get; set; }
         public string lang { get; set; }
 
         public PayByTokenRequest(string transactionId, string token, string invoice, float amount, string lang)
@@ -68,14 +66,12 @@ namespace GolomtEcommerse
             this.invoice = invoice;
             this.amount = amount.ToString("n2");
             this.lang = lang;
-            this.checksum = "";
         }
 
 
         public void computeHash(string secret)
         {
-            string data = amount + transactionId + token;
-            checksum = Utils.GetHMAC(secret, data);
+            this.computeHash(secret, amount + transactionId + token);
         }
     }
 
@@ -85,7 +81,6 @@ namespace GolomtEcommerse
         public string errorDesc { get; set; } = string.Empty;
         public string errorCode { get; set; } = string.Empty;
         public string transactionId { get; set; } = string.Empty;
-        public string checksum { get; set; } = string.Empty;
         public string cardNumber { get; set; } = string.Empty;
     }
 
